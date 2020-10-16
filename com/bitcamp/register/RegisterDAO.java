d4244c302b8e1dc823bcb3c4c20c88e3e69d38e7
jung0226 <jsheemang25@naver.com>
2020-10-16 12:39:50 +0900

    20/10/16

---
+++ com/bitcamp/register/RegisterDAO.java
@@ -0,0 +1,7 @@
+package com.bitcamp.register;
+
+import com.bitcamp.library.DBConnection;
+
+public class RegisterDAO extends DBConnection {
+	
+}
---
+++ com/bitcamp/register/RegisterVO.java
@@ -0,0 +1,10 @@
+package com.bitcamp.register;
+
+public class RegisterVO {
+	private String userid;
+	private String userpwd;
+	private String username;
+	private String gender;
+	private String birth;
+	private String tel;
+}
